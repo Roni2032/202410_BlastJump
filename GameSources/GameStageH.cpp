8e4aa4ca2f305bf4b8a3e33c09c4eef0307724af
Yuya Hashimoto <zeal23420137@fsgcl.jp>
2024-10-22 16:07:11 +0900

    stageHをGameStageと同じように 敵を表示

--- GameSources/GameStageH.cpp
+++ GameSources/GameStageH.cpp
@@ -12,8 +12,8 @@ namespace basecross {
 	//	ゲームステージクラス実体
 	//--------------------------------------------------------------------------------------
 	void GameStageH::CreateViewLight() {
-		const Vec3 eye(0.0f, 5.0f, -5.0f);
-		const Vec3 at(0.0f);
+		const Vec3 eye(0.0f, 10.0f, -30.0f);
+		const Vec3 at(0.0f, 10.0f, 0.0f);
 		auto PtrView = CreateView<SingleView>();
 		//ビューのカメラの設定
 		auto PtrCamera = ObjectFactory::Create<Camera>();
@@ -25,12 +25,69 @@ namespace basecross {
 		//デフォルトのライティングを指定
 		PtrMultiLight->SetDefaultLighting();
 	}
+	void GameStageH::CreateResource() {
+		auto& app = App::GetApp();
+		wstring path = app->GetDataDirWString();
+		wstring uiPath = path + L"UITex/";
+		wstring mapPath = path + L"Maps/";
+		wstring texPath = path + L"Texture/";
+		app->RegisterTexture(L"TEST_TEX", texPath + L"TestTex_wall.jpg");
+		m_CsvMap.SetFileName(mapPath + m_MapName);
+		m_CsvMap.ReadCsv();
+	}
+	void GameStageH::CreateMap() {
 
+		auto& mapVec = m_CsvMap.GetCsvVec();
+		vector<wstring> cells;
+		Vec2 startPos;
+
+		for (int y = 0; y < mapVec.size() - 1; y++) {
+			cells.clear();
+			vector<int> cow;
+			Util::WStrToTokenVector(cells, mapVec[y+1], L',');
+			startPos = Vec2(static_cast<float>(cells.size()) / -2.0f, mapVec.size() - 1);
+
+			for (int x = 0; x < cells.size(); x++) {
+				int cell = stoi(cells[x]);
+				switch (cell) {
+				case 1:
+					AddGameObject<FloorBlock>(L"", Vec3(startPos.x + x, startPos.y - y, 0));
+					break;
+				}
+				cow.push_back(cell);
+			}
+			m_Map.push_back(cow);
+		}
+		Vec2 mapSize = Vec2(cells.size(), mapVec.size() - 1);
+		cells.clear();
+		Util::WStrToTokenVector(cells, mapVec[0], L',');
+
+		Vec2 center = Vec2(0.0f, mapSize.y / 2.0f);
+		//上
+		AddGameObject<Block>(cells[0], Vec3(center.x - 0.5f, startPos.y + 1, 0), Vec3(mapSize.x + 2, 1, 1));
+		//下
+		AddGameObject<Block>(cells[0], Vec3(center.x - 0.5f, startPos.y - mapSize.y, 0), Vec3(mapSize.x + 2, 1, 1));
+		//右
+		AddGameObject<Block>(cells[0], Vec3(startPos.x + mapSize.x, center.y + 0.5f, 0), Vec3(1, mapSize.y, 1));
+		//左
+		AddGameObject<Block>(cells[0], Vec3(startPos.x - 1, center.y + 0.5f, 0), Vec3(1, mapSize.y, 1));
+	}
+
+	void GameStageH::CreateEnemy(){
+		Vec3 AppearancePos = Vec3(0.0f,	0.0f,0.0f);
+		Vec3 Size = Vec3(1.0f);
+		auto enemy = AddGameObject<Enemy>(AppearancePos,Size);
+		//SetSharedGameObject(L"Enemy",enemy);
+	}
 
 
 	void GameStageH::OnCreate() {
 		try {
 			CreateViewLight();
+			CreateResource();
+			CreateMap();
+			CreateEnemy();
+
 		}
 		catch (...) {
 			throw;
--- GameSources/GameStageH.h
+++ GameSources/GameStageH.h
@@ -12,11 +12,18 @@ namespace basecross {
 	//	ゲームステージクラス
 	//--------------------------------------------------------------------------------------
 	class GameStageH : public Stage {
+		vector<vector<int>> m_Map;
+		wstring m_MapName;
+		CsvFile m_CsvMap;
+
 		//ビューの作成
 		void CreateViewLight();
+		void CreateResource();
+		void CreateMap();
+		void CreateEnemy();
 	public:
 		//構築と破棄
-		GameStageH() :Stage() {}
+		GameStageH(const wstring& mapName) : Stage(),m_MapName(mapName) {}
 		virtual ~GameStageH() {}
 		//初期化
 		virtual void OnCreate()override;
