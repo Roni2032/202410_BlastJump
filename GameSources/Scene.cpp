6752554ef2a612754d42c8e62ac50e31976abf56
Munakata Haruto <zeal23420039@fsgcl.jp>
2024-10-29 10:41:26 +0900

    調整

--- GameSources/Scene.cpp
+++ GameSources/Scene.cpp
@@ -33,8 +33,8 @@ namespace basecross{
 	void Scene::OnEvent(const shared_ptr<Event>& event) {
 		if (event->m_MsgStr == L"ToGameStage") {
 			//最初のアクティブステージの設定
-			ResetActiveStage<GameStage>(L"Stage01.csv");
-			//ResetActiveStage<GameStageM>();
+			//ResetActiveStage<GameStage>(L"Stage01.csv");
+			ResetActiveStage<GameStageM>();
 		}
 	}
 
