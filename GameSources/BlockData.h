1e91106193f2a2e498273f9c331e63fa3269869b
Munakata Haruto <zeal23420039@fsgcl.jp>
2024-11-28 12:19:24 +0900

    マップのデータ管理方法を変更

--- GameSources/BlockData.h
+++ GameSources/BlockData.h
@@ -10,13 +10,30 @@ namespace basecross{
 
 	struct BlockData {
 		int m_Id;
+		shared_ptr<GameObject> m_Obj;
 		wstring m_AllData;
 
 		BlockData(int id,const wstring& data = L""):
-			m_Id(id),m_AllData(data){}
+			m_Id(id),
+			m_AllData(data){}
 
 		int GetID() {
 			return m_Id;
+		}
+		void RemoveBlock() {
+			m_Id = 0;
+			ClearGameObject();
+		}
+		shared_ptr<GameObject> GetBlock() {
+			return m_Obj;
+		}
+		void ClearGameObject() {
+			m_Obj = nullptr;
+		}
+		void SetGameObject(const shared_ptr<GameObject>& obj) {
+			if (m_Obj != nullptr) ClearGameObject();
+
+			m_Obj = obj;
 		}
 
 		/*wstring& GetData(const wstring& dataName) {
