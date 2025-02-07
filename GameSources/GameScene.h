b2e41ac6bd67a9ce017f81dbdf5b9016c3dbcc11
Munakata Haruto <zeal23420039@fsgcl.jp>
2025-02-07 10:38:51 +0900

    調整

--- GameSources/GameScene.h
+++ GameSources/GameScene.h
@@ -29,19 +29,19 @@ namespace basecross{
 		//--------------------------------------------------------------------------------------
 		GameScene() :Scene(){
 			m_StageData = {
-				StageData(L"Tutorial01.csv",20,0.0f),
-				StageData(L"Tutorial02.csv",25,0.25f),
-				StageData(L"Tutorial03.csv",20,0.25f),
-				StageData(L"Tutorial04.csv",20,0.25f),
-				StageData(L"Tutorial05.csv",99,0.25f),
-				StageData(L"Tutorial06.csv",99,0.25f),
+				StageData(L"Tutorial01.csv",30,0.0f),
+				StageData(L"Tutorial02.csv",30,0.25f),
+				StageData(L"Tutorial03.csv",30,0.25f),
+				StageData(L"Tutorial04.csv",30,0.25f),
+				StageData(L"Tutorial05.csv",30,0.25f),
+				StageData(L"Tutorial06.csv",30,0.25f),
 
-				StageData(L"Stage01.csv",20,0.25f),
+				StageData(L"Stage01.csv",15,0.25f),
 				StageData(L"Stage02.csv",20,0.25f),
-				StageData(L"Stage03.csv",20,0.25f),
-				StageData(L"Stage04.csv",20,0.25f),
-				StageData(L"Tutorial05.csv",99,0.25f),
-				StageData(L"Stage06.csv",10,0.25f),
+				StageData(L"Stage03.csv",25,0.25f),
+				StageData(L"Stage04.csv",30,0.25f),
+				StageData(L"Stage05.csv",35,0.25f),
+				StageData(L"Stage12.csv",10,0.25f),
 			};
 		}
 		//--------------------------------------------------------------------------------------
